de31a409a12a1cde334e620ba57e3be1fbd90370
Shaikh Irfan <shaikhji1998@gmail.com>
2022-12-04 14:33:31 +0530

    new Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React, {useState, useReducer } from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
+import Provinces from "./Provinces";
 
 // Do not alter the states const and values inside it.
 const states = [
@@ -10,33 +11,33 @@ const states = [
         name: "Indore",
         towns: [
           {
-            name: "Mhow",
+            name: "Mhow"
           },
           {
-            name: "Dewas",
-          },
-        ],
+            name: "Dewas"
+          }
+        ]
       },
       {
         name: "Bhopal",
         towns: [
           {
-            name: "Manit",
+            name: "Manit"
           },
           {
-            name: "Berasia",
-          },
-        ],
+            name: "Berasia"
+          }
+        ]
       },
       {
         name: "Gwalior",
         towns: [
           {
-            name: "Ajaypur",
-          },
-        ],
-      },
-    ],
+            name: "Ajaypur"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Jharkhand",
@@ -45,36 +46,36 @@ const states = [
         name: "Dhanbad",
         towns: [
           {
-            name: "IIT(ISM) Dhanbad",
+            name: "IIT(ISM) Dhanbad"
           },
           {
-            name: "Hirapur",
-          },
-        ],
+            name: "Hirapur"
+          }
+        ]
       },
       {
         name: "Wasseypur",
         towns: [
           {
-            name: "Sardar khan's",
+            name: "Sardar khan's"
           },
           {
-            name: "Faizal khan's",
-          },
-        ],
+            name: "Faizal khan's"
+          }
+        ]
       },
       {
         name: "Mirzapur",
         towns: [
           {
-            name: "Kaleen bhaiya's",
+            name: "Kaleen bhaiya's"
           },
           {
-            name: "Guddu bhaiya's",
-          },
-        ],
-      },
-    ],
+            name: "Guddu bhaiya's"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Assam",
@@ -83,36 +84,36 @@ const states = [
         name: "Guwhati",
         towns: [
           {
-            name: "Amin",
+            name: "Amin"
           },
           {
-            name: "Jalah",
-          },
-        ],
+            name: "Jalah"
+          }
+        ]
       },
       {
         name: "Jungle1",
         towns: [
           {
-            name: "Tiger found at IIT Guwahati",
+            name: "Tiger found at IIT Guwahati"
           },
           {
-            name: "Leopard found in IIT Guwahati",
-          },
-        ],
+            name: "Leopard found in IIT Guwahati"
+          }
+        ]
       },
       {
         name: "Tezpur",
         towns: [
           {
-            name: "Dibrugarh",
+            name: "Dibrugarh"
           },
           {
-            name: "Silchar",
-          },
-        ],
-      },
-    ],
+            name: "Silchar"
+          }
+        ]
+      }
+    ]
   },
   {
     name: "Bihar",
@@ -121,91 +122,53 @@ const states = [
         name: "Patna",
         towns: [
           {
-            name: "Sonpur",
+            name: "Sonpur"
           },
           {
-            name: "Maner",
-          },
-        ],
+            name: "Maner"
+          }
+        ]
       },
       {
         name: "Gaya",
         towns: [
           {
-            name: "Bakraur",
+            name: "Bakraur"
           },
           {
-            name: "Barachatti",
-          },
-        ],
+            name: "Barachatti"
+          }
+        ]
       },
       {
         name: "Darbhanga",
         towns: [
           {
-            name: "Singhwara",
+            name: "Singhwara"
           },
           {
-            name: "Jale",
-          },
-        ],
-      },
-    ],
-  },
+            name: "Jale"
+          }
+        ]
+      }
+    ]
+  }
 ];
 
 function App() {
-  const [state, setState] = useState(1);
-	const [city, setCity] = useState(1);
-	const [town, setTown] = useState(1);
-
-	const handleStateChange = (event) =>{
-		setState(event.target.value);
-		setCity(1);
-		setTown(1);
-	}
-
-	const handleCityChange = (event) =>{
-		setCity(event.target.value);
-		setTown(1);
-	}
-
-	const handleTownChange = (event) =>{
-		setTown(event.target.value);
-	}
-  return <div id="main">
-    <div className="main-section">
-    <div className="dropdown-section">
-				<section id="state" className="dropdown-section state-section">
-					<label>States: </label>
-
-					<select value={state} onChange={handleStateChange}>
-						{
-							states.map((state, index) => <option key={`state-${index}`} value={index}>{state.name}</option>)
-						}
-					</select>
-				</section>
-				<section id="city" className="dropdown-section state-section">
-					<label>Cities: </label>
-
-					<select value={city} onChange={handleCityChange}>
-						{
-							states[state].cities.map((currentCity, index) => <option key={`city-${index}`} value={index}>{currentCity.name}</option>)
-						}
-					</select>
-				</section>
-				<section id="landmark" className="dropdown-section state-section">
-					<label>Towns: </label>
-
-					<select value={town} onChange={handleTownChange}>
-						{
-							states[state].cities[city].towns.map((town, index) => <option key={`town-${index}`} value={index} >{town.name}</option>)
-						}
-					</select>
-				</section>
-			</div>
+  return (
+    <div id="main">
+      <ul>
+        {states.map((item) => (
+          <Provinces
+            index={states.indexOf(item)}
+            name={item.name}
+            cities={item.cities}
+          />
+        ))}
+      </ul>
     </div>
-  </div>;
+  );
 }
 
 export default App;
